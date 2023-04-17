e27fdc2594bc83781eb81c722f882974431410e8
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-04-17 13:37:14 +0800

    feat: add system constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,51 @@
+export const systems = ['nes', 'megadrive', 'vb', 'gb', 'gbc', 'gba', 'gamegear']
+
+export const systemFullNameMap = {
+  gb: 'Nintendo - GameBoy',
+  gba: 'Nintendo - GameBoy Advance',
+  n64: 'Nintendo - Nintendo 64',
+  nes: 'Nintendo - Nintendo Entertainment System',
+  snes: 'Nintendo - Super Nintendo Entertainment System',
+  vb: 'Nintendo - Virtual Boy',
+  gamegear: 'Sega - Game Gear',
+  sms: 'Sega - Master System - Mark III',
+  megadrive: 'Sega - Mega Drive - Genesis',
+  psx: 'Sony - PlayStation',
+}
+
+export const extCoreMap = {
+  nes: 'nestopia',
+  fds: 'nestopia',
+  unf: 'fceumm',
+  unif: 'fceumm',
+  sms: 'genesis_plus_gx',
+  gg: 'genesis_plus_gx',
+  md: 'genesis_plus_gx',
+  '32x': 'picodrive',
+  sfc: 'snes9x',
+  gb: 'gearboy',
+  gbc: 'gearboy',
+  gba: 'mgba',
+  mgw: 'gw',
+  vb: 'beetle_vb',
+  vboy: 'beetle_vb',
+}
+
+export const extSystemMap = {
+  nes: 'nes',
+  fds: 'nes',
+  unf: 'nes',
+  unif: 'nes',
+  sms: 'sms',
+  gg: 'gamegear',
+  md: 'megadrive',
+  '32x': '32x',
+  sfc: 'snes',
+  snes: 'snes',
+  gb: 'gb',
+  gbc: 'gbc',
+  gba: 'gba',
+  mgw: 'gw',
+  vb: 'vb',
+  vboy: 'vb',
+}

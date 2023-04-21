dd5e69d9118315111fc3f563ebfdfddea831444d
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-04-21 21:35:05 +0800

    build: typo

--- scripts/build-retroarch.js
+++ scripts/build-retroarch.js
@@ -2,25 +2,25 @@ import { path7za } from '7zip-bin'
 import node7z from 'node-7z'
 import { $, fs } from 'zx'
 
-const rafileUrl = 'https://buildbot.libretro.com/stable/1.15.0/emscripten/RetroArch.7z'
-const rafileName = 'retroarch.7z'
+const raFileUrl = 'https://buildbot.libretro.com/stable/1.15.0/emscripten/RetroArch.7z'
+const raFileName = 'retroarch.7z'
 
 try {
-  await $`mv node_modules/${rafileName} .`
+  await $`mv node_modules/${raFileName} .`
 } catch {}
 
-if (!(await fs.exists('retroarch')) && !(await fs.exists(rafileName))) {
-  await $`curl ${rafileUrl} -o ${rafileName}`
+if (!(await fs.exists('retroarch')) && !(await fs.exists(raFileName))) {
+  await $`curl ${raFileUrl} -o ${raFileName}`
 }
 
-if (await fs.exists(rafileName)) {
+if (await fs.exists(raFileName)) {
   if (process.platform !== 'win32') {
     await $`chmod +x ${path7za}`
   }
   await new Promise((resolve, reject) =>
-    node7z.extractFull(rafileName, '.', { $bin: path7za }).on('error', reject).on('end', resolve)
+    node7z.extractFull(raFileName, '.', { $bin: path7za }).on('error', reject).on('end', resolve)
   )
-  await $`mv ${rafileName} node_modules`
+  await $`mv ${raFileName} node_modules`
 }
 
 await $`node scripts/generate-cores`

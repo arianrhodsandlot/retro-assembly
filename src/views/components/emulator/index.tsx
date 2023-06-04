a838ca38ce8a240510e0a5a96bc65c1fbf3b82a6
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-06-04 22:47:49 +0800

    feat: menu overlay loading ui

--- src/views/components/emulator/index.tsx
+++ src/views/components/emulator/index.tsx
@@ -1,5 +1,66 @@
+import { AnimatePresence, motion } from 'framer-motion'
+import { useAtom } from 'jotai'
+import { useCallback, useEffect, useRef } from 'react'
+import { createPortal } from 'react-dom'
+import { game, ui } from '../../../core'
+import { showMenuOverlayAtom } from './atoms'
 import { MenuOverlay } from './menu-overlay'
 
+const menuHotButtons = ['l3', 'r3']
+
 export function Emulator() {
-  return <MenuOverlay />
+  const [showMenuOverlay, setShowMenuOverlay] = useAtom(showMenuOverlayAtom)
+  const previousActiveElementRef = useRef<Element | null>(null)
+
+  const toggleMenu = useCallback(() => {
+    if (!game.isRunning()) {
+      return
+    }
+
+    if (showMenuOverlay) {
+      game.start()
+      setShowMenuOverlay(false)
+    } else {
+      game.pause()
+    }
+    setShowMenuOverlay(!showMenuOverlay)
+  }, [showMenuOverlay, setShowMenuOverlay])
+
+  useEffect(() => {
+    function onControlKeyup(event: KeyboardEvent) {
+      if (game.isRunning() && event.key === 'Control') {
+        toggleMenu()
+      }
+    }
+
+    ui.onPressButtons(menuHotButtons, toggleMenu)
+    document.addEventListener('keyup', onControlKeyup)
+
+    if (!showMenuOverlay) {
+      // @ts-expect-error focus previous active element
+      previousActiveElementRef.current?.focus()
+    }
+
+    return () => {
+      ui.offPressButtons(menuHotButtons, toggleMenu)
+      document.removeEventListener('keyup', onControlKeyup)
+    }
+  }, [showMenuOverlay, toggleMenu])
+
+  return createPortal(
+    <AnimatePresence>
+      {showMenuOverlay ? (
+        <motion.div
+          animate={{ opacity: 1 }}
+          className='absolute inset-0 z-[11] overflow-hidden bg-[#00000088] text-white backdrop-blur'
+          exit={{ opacity: 0 }}
+          initial={{ opacity: 0 }}
+          transition={{ duration: 0.1 }}
+        >
+          <MenuOverlay />
+        </motion.div>
+      ) : null}
+    </AnimatePresence>,
+    document.body
+  )
 }
--- src/views/components/emulator/menu-overlay.tsx
+++ src/views/components/emulator/menu-overlay.tsx
@@ -1,69 +1,31 @@
 import { clsx } from 'clsx'
+import { useSetAtom } from 'jotai'
 import { useEffect, useRef, useState } from 'react'
-import { game, ui } from '../../../core'
+import { game } from '../../../core'
 import { emitter } from '../../lib/emitter'
+import { showMenuOverlayAtom } from './atoms'
 import { StatesList } from './states-list'
 
-const menuHotButtons = ['l3', 'r3']
-
 export function MenuOverlay() {
-  const [show, setShow] = useState(false)
+  const setShowMenuOverlay = useSetAtom(showMenuOverlayAtom)
   const [showStateList, setShowStateList] = useState(false)
   const firstButtonRef = useRef<HTMLButtonElement>(null)
-  const previousActiveElementRef = useRef<Element | null>(null)
   const [isLoadingState, setIsLoadingState] = useState(false)
-
-  useEffect(() => {
-    function toggleMenu() {
-      if (!game.isRunning()) {
-        return
-      }
-
-      if (show) {
-        resume()
-      } else {
-        game.pause()
-      }
-      setShow(!show)
-    }
-
-    function onControlKeyup(event: KeyboardEvent) {
-      if (game.isRunning() && event.key === 'Control') {
-        toggleMenu()
-      }
-    }
-
-    ui.onPressButtons(menuHotButtons, toggleMenu)
-    document.addEventListener('keyup', onControlKeyup)
-
-    if (show) {
-      previousActiveElementRef.current = document.activeElement
-      firstButtonRef.current?.focus()
-    } else {
-      // @ts-expect-error focus previous active element
-      previousActiveElementRef.current?.focus()
-    }
-
-    return () => {
-      ui.offPressButtons(menuHotButtons, toggleMenu)
-      document.removeEventListener('keyup', onControlKeyup)
-    }
-  }, [show])
 
   async function saveState() {
     await game.saveState()
     game.start()
-    setShow(false)
+    setShowMenuOverlay(false)
   }
 
   function resume() {
     game.start()
-    setShow(false)
+    setShowMenuOverlay(false)
   }
 
   function exit() {
     game.exit()
-    setShow(false)
+    setShowMenuOverlay(false)
     emitter.emit('exit')
   }
 
@@ -71,62 +33,58 @@ export function MenuOverlay() {
     setIsLoadingState(true)
     await game.loadState(stateId)
     setIsLoadingState(false)
-    setShow(false)
+    setShowMenuOverlay(false)
   }
 
-  if (!show) {
-    return null
-  }
-
-  if (isLoadingState) {
-    return (
-      <div className='absolute inset-0 z-[11] flex items-center justify-center bg-[#00000055] text-white backdrop-blur'>
-        <span className='icon-[line-md--loading-loop] h-12 w-12 text-white' />
-      </div>
-    )
-  }
+  useEffect(() => {
+    firstButtonRef.current?.focus()
+  }, [])
 
   const menuButtonClassNames =
     'py-4 pr-20 text-right transition-[color,background-color] focus:bg-white focus:text-red-600 flex items-center justify-end'
 
   return (
-    <div className='menu-overlay absolute inset-0 z-[11] flex justify-center bg-[#00000055] text-white backdrop-blur'>
-      <div className='menu-overlay-buttons w-1/2'>
-        <div className='relative h-full w-full py-10 text-xl'>
-          <div className='absolute inset-y-10 flex w-full flex-col justify-center border-r-2 border-r-white'>
-            <button
-              className={menuButtonClassNames}
-              onClick={resume}
-              onFocus={() => setShowStateList(false)}
-              ref={firstButtonRef}
-            >
-              <span className='icon-[material-symbols--resume] mr-2 h-6 w-6' />
-              Resume
-            </button>
+    <div className='menu-overlay flex h-full w-full items-stretch justify-center py-10'>
+      {isLoadingState ? (
+        <span className='icon-[line-md--loading-loop] h-12 w-12 text-white' />
+      ) : (
+        <>
+          <div className='menu-overlay-buttons flex w-1/2 items-center border-r-2 border-r-white'>
+            <div className='absolut inset-y-10 flex w-full flex-col justify-center text-xl'>
+              <button
+                className={menuButtonClassNames}
+                onClick={resume}
+                onFocus={() => setShowStateList(false)}
+                ref={firstButtonRef}
+              >
+                <span className='icon-[material-symbols--resume] mr-2 h-6 w-6' />
+                Resume
+              </button>
 
-            <button className={menuButtonClassNames} onClick={saveState} onFocus={() => setShowStateList(false)}>
-              <span className='icon-[mdi--content-save] mr-2 h-6 w-6' />
-              Save state
-            </button>
+              <button className={menuButtonClassNames} onClick={saveState} onFocus={() => setShowStateList(false)}>
+                <span className='icon-[mdi--content-save] mr-2 h-6 w-6' />
+                Save state
+              </button>
 
-            <button
-              className={clsx(menuButtonClassNames, { 'bg-white text-red-600': showStateList })}
-              onFocus={() => setShowStateList(true)}
-            >
-              <span className='icon-[mdi--tray-arrow-down] mr-2 h-6 w-6' />
-              Load state
-            </button>
+              <button
+                className={clsx(menuButtonClassNames, { 'bg-white text-red-600': showStateList })}
+                onFocus={() => setShowStateList(true)}
+              >
+                <span className='icon-[mdi--tray-arrow-down] mr-2 h-6 w-6' />
+                Load state
+              </button>
 
-            <button className={menuButtonClassNames} onClick={exit} onFocus={() => setShowStateList(false)}>
-              <span className='icon-[mdi--exit-to-app] mr-2 h-6 w-6' />
-              Exit
-            </button>
+              <button className={menuButtonClassNames} onClick={exit} onFocus={() => setShowStateList(false)}>
+                <span className='icon-[mdi--exit-to-app] mr-2 h-6 w-6' />
+                Exit
+              </button>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className='menu-overlay-button-details w-1/2'>
-        {showStateList ? <StatesList onSelect={onSelectState} /> : null}
-      </div>
+          <div className='menu-overlay-button-details w-1/2'>
+            {showStateList ? <StatesList onSelect={onSelectState} /> : null}
+          </div>
+        </>
+      )}
     </div>
   )
 }

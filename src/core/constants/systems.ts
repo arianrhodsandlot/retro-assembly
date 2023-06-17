fd914f0a1920351e978cc6f39c828edf4e7ab076
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-06-17 14:30:25 +0800

    fix: remove unsuported systems

--- src/core/constants/systems.ts
+++ src/core/constants/systems.ts
@@ -1,14 +1,10 @@
 export type SystemName =
-  | '32x'
   | 'gamegear'
   | 'gb'
   | 'gba'
   | 'gbc'
-  | 'gw'
   | 'megadrive'
-  | 'n64'
   | 'nes'
-  | 'psx'
   | 'sms'
   | 'snes'
   | 'vb'
@@ -34,16 +30,13 @@ type CoreName =
   | 'prosystem'
   | 'mednafen_lynx'
 
-// systems are currently sorted based upon their popularity, see https://en.wikipedia.org/wiki/List_of_best-selling_game_consoles
+// systems are sorted based upon their popularity, see https://en.wikipedia.org/wiki/List_of_best-selling_game_consoles
 export const systemNamesSorted: SystemName[] = [
   'gb',
   'gbc',
   'gba',
-  'psx',
   'nes',
   'snes',
-  'gw',
-  'n64',
   'megadrive',
   'atari2600',
   'sms',
@@ -51,21 +44,16 @@ export const systemNamesSorted: SystemName[] = [
   'atari7800',
   'atarilynx',
   'atari5200',
-  '32x',
   'vb',
 ]
 
 export const systemFullNameMap: Record<SystemName, string> = {
-  '32x': 'Sega - 32X',
-  gw: 'Handheld Electronic Game',
   gamegear: 'Sega - Game Gear',
   gb: 'Nintendo - Game Boy',
   gba: 'Nintendo - Game Boy Advance',
   gbc: 'Nintendo - Game Boy Color',
   megadrive: 'Sega - Mega Drive - Genesis',
-  n64: 'Nintendo - Nintendo 64',
   nes: 'Nintendo - Nintendo Entertainment System',
-  psx: 'Sony - PlayStation',
   sms: 'Sega - Master System - Mark III',
   snes: 'Nintendo - Super Nintendo Entertainment System',
   vb: 'Nintendo - Virtual Boy',
@@ -76,16 +64,12 @@ export const systemFullNameMap: Record<SystemName, string> = {
 }
 
 export const systemCoreMap: Record<SystemName, CoreName> = {
-  '32x': 'picodrive',
   gamegear: 'genesis_plus_gx',
   gb: 'gearboy',
   gba: 'mgba',
   gbc: 'gearboy',
-  gw: 'gw',
   megadrive: 'genesis_plus_gx',
-  n64: 'mupen64plus_next',
   nes: 'nestopia',
-  psx: 'pcsx2',
   sms: 'genesis_plus_gx',
   snes: 'snes9x',
   vb: 'beetle_vb',
@@ -96,14 +80,12 @@ export const systemCoreMap: Record<SystemName, CoreName> = {
 }
 
 export const extSystemMap: Record<string, SystemName> = {
-  '32x': '32x',
   fds: 'nes',
   gb: 'gb',
   gba: 'gba',
   gbc: 'gbc',
   gg: 'gamegear',
   md: 'megadrive',
-  mgw: 'gw',
   nes: 'nes',
   sfc: 'snes',
   sms: 'sms',

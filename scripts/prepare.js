4856d49fbf04608f6ae1f8e23ab3b88a049da8cb
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-06-18 19:18:34 +0800

    fix: fix prepare script

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -2,7 +2,7 @@
 import { $, cd } from 'zx'
 
 cd('src')
-await $`curl -O https://github.com/arianrhodsandlot/retro-assembly-vendors/releases/download/v202306181619/retro-assembly-vendor.zip`
-await $`mkdir vendor`
+await $`curl -LO https://github.com/arianrhodsandlot/retro-assembly-vendors/releases/download/v202306181619/retro-assembly-vendor.zip`
+await $`mkdir -p vendor`
 await $`unzip -q retro-assembly-vendor.zip -d vendor`
 await $`mv retro-assembly-vendor.zip vendor`

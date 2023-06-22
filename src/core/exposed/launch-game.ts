664d9003e21c5e7bb505d5d7064eb0025bab0be0
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-06-22 12:09:00 +0800

    fix: force exit game before launching

--- src/core/exposed/launch-game.ts
+++ src/core/exposed/launch-game.ts
@@ -1,11 +1,16 @@
 import { Emulator } from '../classes/emulator'
 import { type Rom } from '../classes/rom'
 import { globalContext } from '../internal/global-context'
+import { exitGame } from './exit-game'
 
 export async function launchGame(rom: Rom) {
   if (!globalContext.fileSystem) {
     throw new Error('fileSystem is not available')
   }
+
+  try {
+    exitGame()
+  } catch {}
 
   const emulator = new Emulator({ rom })
   globalContext.emulator = emulator

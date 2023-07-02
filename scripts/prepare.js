7a147b376e73a376ab7b6e693237d11075137404
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-07-02 22:57:09 +0800

    fix: remove vendor zip file after it has been extracted

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -5,4 +5,4 @@ cd('src')
 await $`curl -LO https://github.com/arianrhodsandlot/retro-assembly-vendors/releases/download/v202306182202/retro-assembly-vendor.zip`
 await $`mkdir -p vendor`
 await $`unzip -q retro-assembly-vendor.zip -d vendor`
-await $`mv retro-assembly-vendor.zip vendor`
+await $`rm retro-assembly-vendor.zip`

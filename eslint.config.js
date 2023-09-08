98cfc50f0515a047219c8e9ae53a8295c6cbb3b2
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-09-08 23:28:54 +0800

    chore: update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,9 @@
 import { createConfig } from '@arianrhodsandlot/eslint-config'
 
 export default createConfig({
-  append: [{
-    ignores: [
-      'src/core/classes/libretrodb',
-      'src/views/lib/spatial-navigation/js-spatial-navigation.ts'
-    ]
-  }]
+  append: [
+    {
+      ignores: ['src/core/classes/libretrodb', 'src/views/lib/spatial-navigation/js-spatial-navigation.ts'],
+    },
+  ],
 })

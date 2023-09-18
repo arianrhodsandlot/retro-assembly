6cc0f2a38cb3218373ad942539f706b4f04b4b04
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-09-18 17:55:21 +0800

    chore: update cores version

--- src/core/constants/dependencies.ts
+++ src/core/constants/dependencies.ts
@@ -2,15 +2,15 @@ export const cdnHost = 'https://cdn.jsdelivr.net'
 
 export const vendorsInfo = {
   name: 'retro-assembly-vendors',
-  version: '1.16.0-202309051511',
+  version: '1.16.0-202309181642',
 }
 
 export const libretroDatabaseInfo = {
   name: 'libretro/libretro-database',
-  version: '213f2dc',
+  version: '7f32417',
 }
 
 export const fbneoInfo = {
   name: 'libretro/FBNeo',
-  version: '49fedb7',
+  version: '6b6ba33',
 }

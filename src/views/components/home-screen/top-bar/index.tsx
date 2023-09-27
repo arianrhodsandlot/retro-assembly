4d885dbfeafabba8ad2bd1ebc6124294fb1a6d2d
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-09-27 10:01:40 +0800

    test: add some basic test cases

--- src/views/components/home-screen/top-bar/index.tsx
+++ src/views/components/home-screen/top-bar/index.tsx
@@ -8,7 +8,10 @@ export function TopBar() {
   const systems = useAtomValue(systemsAtom)
 
   return (
-    <div className='system-navigation z-[1] flex h-16 w-full items-stretch overflow-x-auto overflow-y-hidden border-b border-black bg-rose-700 text-white'>
+    <div
+      className='system-navigation z-[1] flex h-16 w-full items-stretch overflow-x-auto overflow-y-hidden border-b border-black bg-rose-700 text-white'
+      data-testid='system-navigation'
+    >
       <Logo />
       {systems?.length ? <SystemNavigation /> : <div className='flex-1' />}
       <TopBarDropdown />
--- tests/e2e/setup-wizard/single-rom.spec.ts
+++ tests/e2e/setup-wizard/single-rom.spec.ts
@@ -2,13 +2,15 @@ import { expect, test } from '@playwright/test'
 
 test('play a single ROM file', async ({ page }) => {
   await page.goto('/')
+
+  const emulator = page.getByTestId('emulator')
+  await expect(emulator).not.toBeAttached()
+
   const fileChooserPromise = page.waitForEvent('filechooser')
   await page.getByTestId('select-a-rom').click()
   const fileChooser = await fileChooserPromise
   await fileChooser.setFiles('tests/fixtures/roms/nes/240p Test Suite.nes')
 
-  const emulator = page.getByTestId('emulator')
   await page.waitForLoadState('networkidle')
   await expect(emulator).toBeVisible()
-  await expect(emulator).toHaveScreenshot('240p Test Suite.png')
 })

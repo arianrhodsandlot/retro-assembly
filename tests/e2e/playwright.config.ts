b10969365bdfbf2ed3e2301f363eb890c9b1e77b
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-09-27 11:44:19 +0800

    test: fix test in ci

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -1,16 +1,13 @@
-import process from 'node:process'
 import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   testDir: '.',
   snapshotDir: 'snapshots',
   snapshotPathTemplate: '{snapshotDir}/{testFilePath}/{testName}-{arg}{ext}',
-  timeout: 5 * 60 * 1000,
   use: {
     baseURL: 'http://localhost:5173',
     trace: 'on-first-retry',
-    headless: Boolean(process.env.CI),
   },
-  webServer: { command: 'pnpm dev', url: 'http://localhost:5173', reuseExistingServer: true },
+  webServer: { command: 'pnpm dev', url: 'http://localhost:5173', reuseExistingServer: !process.env.CI },
   expect: { toHaveScreenshot: { maxDiffPixelRatio: 0.05 } },
 })

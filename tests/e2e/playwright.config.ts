703115dd661bdef70d39f02e48b657516a2208f7
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-10-01 12:03:55 +0800

    test: remove useless snapshots

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from '@playwright/test'
+import isCi from 'is-ci'
 
 export default defineConfig({
-  testDir: '.',
-  snapshotDir: 'snapshots',
-  snapshotPathTemplate: '{snapshotDir}/{testFilePath}/{testName}-{arg}{ext}',
   use: {
     baseURL: 'http://localhost:5173',
-    trace: 'on-first-retry',
   },
-  webServer: { command: 'pnpm dev', url: 'http://localhost:5173', reuseExistingServer: !process.env.CI },
-  expect: { toHaveScreenshot: { maxDiffPixelRatio: 0.05 } },
+  webServer: {
+    command: 'pnpm dev',
+    port: 5173,
+    reuseExistingServer: !isCi,
+  },
 })

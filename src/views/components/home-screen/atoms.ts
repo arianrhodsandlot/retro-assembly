54c4c3b25d0429d18f7e5fa0534256d0e74403b3
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-10-15 21:13:28 +0800

    fix: fix bug when resizing window while launching a game

--- src/views/components/home-screen/atoms.ts
+++ src/views/components/home-screen/atoms.ts
@@ -1,6 +1,14 @@
 import { atom } from 'jotai'
+import { type MouseEvent } from 'react'
 import { type Rom, type SystemName } from '../../../core'
 
 export const currentSystemNameAtom = atom('')
 export const romsAtom = atom<Rom[]>([])
 export const systemsAtom = atom<{ name: SystemName; fullName: string }[]>([])
+
+interface MaskAtomValue {
+  event: MouseEvent<HTMLButtonElement>
+  target: HTMLButtonElement
+  rom: Rom
+}
+export const maskAtom = atom<MaskAtomValue | undefined>(undefined)

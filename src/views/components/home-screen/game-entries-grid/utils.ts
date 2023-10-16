80c6b0f236b24caedd621d8aa54698b52a7c5293
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-10-16 20:28:46 +0800

    fix: fix cancelable image loading

--- src/views/components/home-screen/game-entries-grid/utils.ts
+++ src/views/components/home-screen/game-entries-grid/utils.ts
@@ -1,13 +1,32 @@
+import ky from 'ky'
 import PQueue from 'p-queue'
-
-const emptyImage = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs='
 
 const queue = new PQueue({ concurrency: 5 })
 
-async function loadImage(src: string, img: HTMLImageElement) {
+const validImages = {}
+const invalidImages = {}
+async function loadImage(src: string, signal: AbortSignal) {
+  if (src in validImages) {
+    return
+  }
+  if (src in invalidImages) {
+    throw new Error('invalid image')
+  }
+
+  try {
+    await ky.get(src, { signal, timeout: false })
+  } catch (error: any) {
+    if (typeof error?.response?.status === 'number') {
+      invalidImages[src] = true
+    }
+    throw error
+  }
+
+  const img = new Image()
   img.src = src
   return await new Promise<void>((resolve, reject) => {
     img.addEventListener('load', () => {
+      validImages[src] = true
       resolve()
     })
     img.addEventListener('error', (error) => {
@@ -16,17 +35,8 @@ async function loadImage(src: string, img: HTMLImageElement) {
   })
 }
 
-export function loadImageWithLimit(src: string, signal: AbortController['signal']) {
-  return queue.add(
-    async () => {
-      const img = new Image()
-      signal.addEventListener('abort', () => {
-        img.src = emptyImage
-      })
-      await loadImage(src, img)
-    },
-    { signal },
-  )
+export function loadImageWithLimit(src: string, signal: AbortSignal) {
+  return queue.add(() => loadImage(src, signal), { signal })
 }
 
 export function clearLoadImageQueue() {

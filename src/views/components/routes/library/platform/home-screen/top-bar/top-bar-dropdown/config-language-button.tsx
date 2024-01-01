b5d8e4a8fb4929ebf776d7ec7770a20a97b00ae6
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2024-01-01 20:13:50 +0800

    feat: add translation select

---
+++ src/views/components/routes/library/platform/home-screen/top-bar/top-bar-dropdown/config-language-button.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { BaseDialogTrigger } from '../../../../../../primitives/base-dialog-trigger'
+import { TopBarButton } from '../top-bar-button'
+import { ConfigLanguageDialogContent } from './config-language-dialog-content'
+
+export function ConfigLanguageButton() {
+  const { t } = useTranslation()
+  const [open, setOpen] = useState(false)
+
+  function onOpenChange(open) {
+    setOpen(open)
+  }
+
+  return (
+    <BaseDialogTrigger content={<ConfigLanguageDialogContent />} onOpenChange={onOpenChange} open={open}>
+      <TopBarButton>
+        <div className='flex items-center gap-2 px-4'>
+          <span className='icon-[mdi--translate-variant] relative z-[1] h-8 w-8' />
+          {t('Language')}
+        </div>
+      </TopBarButton>
+    </BaseDialogTrigger>
+  )
+}

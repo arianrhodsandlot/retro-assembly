9f3b93addab4c8b1ff3273dd4cebe06c76008a99
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2024-02-25 19:04:42 +0800

    fix: remove useless files

--- src/core/helpers/vendors.ts
+++ src/core/helpers/vendors.ts
@@ -1,4 +1,7 @@
 import { Nostalgist } from 'nostalgist'
 
-export const { Buffer } = Nostalgist.vendors.browserfs.BFSRequire('buffer')
-export const path = Nostalgist.vendors.browserfs.BFSRequire('path')
+const { browserfs } = Nostalgist.vendors
+const { BFSRequire } = browserfs
+
+export const { Buffer } = BFSRequire('buffer')
+export const path = BFSRequire('path')
